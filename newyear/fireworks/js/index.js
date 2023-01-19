6f67e558f2e22ac4b9e3b1c7404b4163951fc9a6
DANSmartCloud <DANzhiyun2020@foxmail.com>
2023-01-19 12:06:32 +0800

    com wait pic

--- newyear/fireworks/js/index.js
+++ newyear/fireworks/js/index.js
@@ -21,35 +21,35 @@ var Fireworks = function(){
 			return false;
 		};
 		
-		self.canvas.width = self.cw = 600;
-		self.canvas.height = self.ch = 400;	
+		self.canvas.width = self.cw = 1920;
+		self.canvas.height = self.ch =1080;	
 		
 		self.particles = [];	
-		self.partCount = 30;
+		self.烟花散开数量 = 30;
 		self.fireworks = [];	
 		self.mx = self.cw/2;
 		self.my = self.ch/2;
 		self.currentHue = 170;
-		self.partSpeed = 5;
-		self.partSpeedVariance = 10;
-		self.partWind = 50;
-		self.partFriction = 5;
-		self.partGravity = 1;
-		self.hueMin = 150;
-		self.hueMax = 200;
+		self.速度 = 5;
+		self.烟花散开速度差 = 10;
+		self.部件风速 = 50;
+		self.烟花散开阻力 = 5;
+		self.重力方向 = 1;
+		self.色调最小值 = 150;
+		self.色调最大值 = 200;
 		self.发射速度 = 2;
 		self.发射速率 = 4;
-		self.hueVariance = 30;
-		self.flickerDensity = 20;
+		self.色调差值 = 30;
+		self.烟花密度 = 20;
 		self.冲击波 = false;
 		self.目标圆环 = true;
-		self.clearAlpha = 25;
+		self.消失时长 = 25;
 
 		self.canvasContainer.append(self.canvas);
 		self.ctx = self.canvas.getContext('2d');
 		self.ctx.lineCap = 'round';
 		self.ctx.lineJoin = 'round';
-		self.lineWidth = 1;
+		self.烟花粗细 = 1;
 		self.bindEvents();			
 		self.canvasLoop();
 		
@@ -72,15 +72,15 @@ var Fireworks = function(){
 			{x: x, y: y}
 		];
 		this.angle = rand(0, 360);
-		this.speed = rand(((self.partSpeed - self.partSpeedVariance) <= 0) ? 1 : self.partSpeed - self.partSpeedVariance, (self.partSpeed + self.partSpeedVariance));
-		this.friction = 1 - self.partFriction/100;
-		this.gravity = self.partGravity/2;
-		this.hue = rand(hue-self.hueVariance, hue+self.hueVariance);
+		this.speed = rand(((self.速度 - self.烟花散开速度差) <= 0) ? 1 : self.速度 - self.烟花散开速度差, (self.速度 + self.烟花散开速度差));
+		this.friction = 1 - self.烟花散开阻力/100;
+		this.gravity = self.重力方向/2;
+		this.hue = rand(hue-self.色调差值, hue+self.色调差值);
 		this.brightness = rand(50, 80);
 		this.alpha = rand(40,100)/100;
 		this.decay = rand(10, 50)/1000;
-		this.wind = (rand(0, self.partWind) - (self.partWind/2))/25;
-		this.lineWidth = self.lineWidth;
+		this.wind = (rand(0, self.部件风速) - (self.部件风速/2))/25;
+		this.烟花粗细 = self.烟花粗细;
 	};
 	
 	Particle.prototype.update = function(index){
@@ -116,11 +116,11 @@ var Fireworks = function(){
 		self.ctx.strokeStyle = 'hsla('+this.hue+', 100%, '+this.brightness+'%, '+this.alpha+')';
 		self.ctx.stroke();				
 		
-		if(self.flickerDensity > 0){
-			var inverseDensity = 50 - self.flickerDensity;					
+		if(self.烟花密度 > 0){
+			var inverseDensity = 50 - self.烟花密度;					
 			if(rand(0, inverseDensity) === inverseDensity){
 				self.ctx.beginPath();
-				self.ctx.arc(Math.round(this.x), Math.round(this.y), rand(this.lineWidth,this.lineWidth+3)/2, 0, Math.PI*2, false)
+				self.ctx.arc(Math.round(this.x), Math.round(this.y), rand(this.烟花粗细,this.烟花粗细+3)/2, 0, Math.PI*2, false)
 				self.ctx.closePath();
 				var randAlpha = rand(50,100)/100;
 				self.ctx.fillStyle = 'hsla('+this.hue+', 100%, '+this.brightness+'%, '+randAlpha+')';
@@ -133,7 +133,7 @@ var Fireworks = function(){
 	/* Create Particles
 	/*=============================================================================*/
 	self.createParticles = function(x,y, hue){
-		var countdown = self.partCount;
+		var countdown = self.烟花散开数量;
 		while(countdown--){						
 			self.particles.push(new Particle(x, y, hue));
 		}
@@ -185,12 +185,12 @@ var Fireworks = function(){
 		this.hue = self.currentHue;
 		this.brightness = rand(50, 80);
 		this.alpha = rand(50,100)/100;
-		this.lineWidth = self.lineWidth;
+		this.烟花粗细 = self.烟花粗细;
 		this.targetRadius = 1;
 	};
 	
 	Firework.prototype.update = function(index){
-		self.ctx.lineWidth = this.lineWidth;
+		self.ctx.烟花粗细 = this.烟花粗细;
 			
 		vx = Math.cos(this.angle) * this.speed,
 		vy = Math.sin(this.angle) * this.speed;
@@ -250,7 +250,7 @@ var Fireworks = function(){
 	};
 	
 	Firework.prototype.draw = function(){
-		self.ctx.lineWidth = this.lineWidth;
+		self.ctx.烟花粗细 = this.烟花粗细;
 			
 		var coordRand = (rand(1,3)-1);					
 		self.ctx.beginPath();							
@@ -265,7 +265,7 @@ var Fireworks = function(){
 			self.ctx.beginPath();
 			self.ctx.arc(Math.round(this.targetX), Math.round(this.targetY), this.targetRadius, 0, Math.PI*2, false)
 			self.ctx.closePath();
-			self.ctx.lineWidth = 1;
+			self.ctx.烟花粗细 = 1;
 			self.ctx.stroke();
 			self.ctx.restore();
 		}
@@ -277,7 +277,7 @@ var Fireworks = function(){
 			self.ctx.beginPath();
 			self.ctx.arc(0, 0, 1*(this.speed/5), 0, Math.PI, true);
 			self.ctx.strokeStyle = 'hsla('+this.hue+', 100%, '+this.brightness+'%, '+rand(25, 60)/100+')';
-			self.ctx.lineWidth = this.lineWidth;
+			self.ctx.烟花粗细 = this.烟花粗细;
 			self.ctx.stroke();
 			self.ctx.restore();
 		}								 
@@ -328,14 +328,14 @@ var Fireworks = function(){
 			var randLaunch = rand(0, 5);
 			self.mx = e.pageX - self.canvasContainer.offset().left;
 			self.my = e.pageY - self.canvasContainer.offset().top;
-			self.currentHue = rand(self.hueMin, self.hueMax);
+			self.currentHue = rand(self.色调最小值, self.色调最大值);
 			self.createFireworks(self.cw/2, self.ch, self.mx, self.my);	
 			
 			$(self.canvas).on('mousemove.fireworks', function(e){
 				var randLaunch = rand(0, 5);
 				self.mx = e.pageX - self.canvasContainer.offset().left;
 				self.my = e.pageY - self.canvasContainer.offset().top;
-				self.currentHue = rand(self.hueMin, self.hueMax);
+				self.currentHue = rand(self.色调最小值, self.色调最大值);
 				self.createFireworks(self.cw/2, self.ch, self.mx, self.my);									
 			});	
 			
@@ -373,7 +373,7 @@ var Fireworks = function(){
 		requestAnimFrame(self.canvasLoop, self.canvas);
     self.updateDelta();
 		self.ctx.globalCompositeOperation = 'destination-out';
-		self.ctx.fillStyle = 'rgba(0,0,0,'+self.clearAlpha/100+')';
+		self.ctx.fillStyle = 'rgba(0,0,0,'+self.消失时长/100+')';
 		self.ctx.fillRect(0,0,self.cw,self.ch);
 		self.ctx.globalCompositeOperation = 'lighter';
 		self.updateFireworks();
@@ -405,18 +405,18 @@ var guiPresets = {
 					"发射速率": 4,
 					"冲击波": false,
 					"目标圆环": true,
-					"partCount": 30,
-					"partSpeed": 5,
-					"partSpeedVariance": 10,
-					"partWind": 50,
-					"partFriction": 5,
-					"partGravity": 1,
-					"flickerDensity": 20,
-					"hueMin": 150,
-					"hueMax": 200,
-					"hueVariance": 30,
-					"lineWidth": 1,
-					"clearAlpha": 25
+					"烟花散开数量": 30,
+					"速度": 5,
+					"烟花散开速度差": 10,
+					"部件风速": 50,
+					"烟花散开阻力": 5,
+					"重力方向": 1,
+					"烟花密度": 20,
+					"色调最小值": 150,
+					"色调最大值": 200,
+					"色调差值": 30,
+					"烟花粗细": 1,
+					"消失时长": 25
 				  }
 				},
 				"反重力": {
@@ -425,18 +425,18 @@ var guiPresets = {
 					"发射速率": 10,
 					"冲击波": true,
 					"目标圆环": false,
-					"partCount": 150,
-					"partSpeed": 5,
-					"partSpeedVariance": 10,
-					"partWind": 10,
-					"partFriction": 10,
-					"partGravity": -10,
-					"flickerDensity": 30,
-					"hueMin": 0,
-					"hueMax": 360,
-					"hueVariance": 30,
-					"lineWidth": 1,
-					"clearAlpha": 50
+					"烟花散开数量": 150,
+					"速度": 5,
+					"烟花散开速度差": 10,
+					"部件风速": 10,
+					"烟花散开阻力": 10,
+					"重力方向": -10,
+					"烟花密度": 30,
+					"色调最小值": 0,
+					"色调最大值": 360,
+					"色调差值": 30,
+					"烟花粗细": 1,
+					"消失时长": 50
 				  }
 				},
 				"大金闪花": {
@@ -445,18 +445,18 @@ var guiPresets = {
 					"发射速率": 20,
 					"冲击波": true,
 					"目标圆环": true,
-					"partCount": 200,
-					"partSpeed": 30,
-					"partSpeedVariance": 5,
-					"partWind": 0,
-					"partFriction": 5,
-					"partGravity": 0,
-					"flickerDensity": 0,
-					"hueMin": 20,
-					"hueMax": 30,
-					"hueVariance": 10,
-					"lineWidth": 1,
-					"clearAlpha": 40
+					"烟花散开数量": 200,
+					"速度": 30,
+					"烟花散开速度差": 5,
+					"部件风速": 0,
+					"烟花散开阻力": 5,
+					"重力方向": 0,
+					"烟花密度": 0,
+					"色调最小值": 20,
+					"色调最大值": 30,
+					"色调差值": 10,
+					"烟花粗细": 1,
+					"消失时长": 40
 				  }
 				},
 				"圆头照明烟花": {
@@ -465,18 +465,18 @@ var guiPresets = {
 					"发射速率": 3,
 					"冲击波": true,
 					"目标圆环": true,
-					"partCount": 500,
-					"partSpeed": 50,
-					"partSpeedVariance": 5,
-					"partWind": 0,
-					"partFriction": 0,
-					"partGravity": 0,
-					"flickerDensity": 0,
-					"hueMin": 0,
-					"hueMax": 360,
-					"hueVariance": 30,
-					"lineWidth": 20,
-					"clearAlpha": 20
+					"烟花散开数量": 500,
+					"速度": 50,
+					"烟花散开速度差": 5,
+					"部件风速": 0,
+					"烟花散开阻力": 0,
+					"重力方向": 0,
+					"烟花密度": 0,
+					"色调最小值": 0,
+					"色调最大值": 360,
+					"色调差值": 30,
+					"烟花粗细": 20,
+					"消失时长": 20
 				  }
 				},
 				"彩珠筒": {
@@ -485,18 +485,18 @@ var guiPresets = {
 					"发射速率": 50,
 					"冲击波": false,
 					"目标圆环": false,
-					"partCount": 120,
-					"partSpeed": 10,
-					"partSpeedVariance": 10,
-					"partWind": 100,
-					"partFriction": 50,
-					"partGravity": 0,
-					"flickerDensity": 20,
-					"hueMin": 0,
-					"hueMax": 360,
-					"hueVariance": 30,
-					"lineWidth": 1,
-					"clearAlpha": 80
+					"烟花散开数量": 120,
+					"速度": 10,
+					"烟花散开速度差": 10,
+					"部件风速": 100,
+					"烟花散开阻力": 50,
+					"重力方向": 0,
+					"烟花密度": 20,
+					"色调最小值": 0,
+					"色调最大值": 360,
+					"色调差值": 30,
+					"烟花粗细": 1,
+					"消失时长": 80
 				  }
 				},
 				"树枝式": {
@@ -505,18 +505,18 @@ var guiPresets = {
 					"发射速率": 2,
 					"冲击波": false,
 					"目标圆环": false,
-					"partCount": 200,
-					"partSpeed": 10,
-					"partSpeedVariance": 0,
-					"partWind": 100,
-					"partFriction": 0,
-					"partGravity": 2,
-					"flickerDensity": 50,
-					"hueMin": 0,
-					"hueMax": 360,
-					"hueVariance": 20,
-					"lineWidth": 4,
-					"clearAlpha": 10
+					"烟花散开数量": 200,
+					"速度": 10,
+					"烟花散开速度差": 0,
+					"部件风速": 100,
+					"烟花散开阻力": 0,
+					"重力方向": 2,
+					"烟花密度": 50,
+					"色调最小值": 0,
+					"色调最大值": 360,
+					"色调差值": 20,
+					"烟花粗细": 4,
+					"消失时长": 10
 				  }
 				},
 				"定格": {
@@ -525,18 +525,18 @@ var guiPresets = {
 					"发射速率": 10,
 					"冲击波": false,
 					"目标圆环": false,
-					"partCount": 150,
-					"partSpeed": 10,
-					"partSpeedVariance": 10,
-					"partWind": 100,
-					"partFriction": 3,
-					"partGravity": 0,
-					"flickerDensity": 0,
-					"hueMin": 0,
-					"hueMax": 360,
-					"hueVariance": 20,
-					"lineWidth": 1,
-					"clearAlpha": 0
+					"烟花散开数量": 150,
+					"速度": 10,
+					"烟花散开速度差": 10,
+					"部件风速": 100,
+					"烟花散开阻力": 3,
+					"重力方向": 0,
+					"烟花密度": 0,
+					"色调最小值": 0,
+					"色调最大值": 360,
+					"色调差值": 20,
+					"烟花粗细": 1,
+					"消失时长": 0
 				  }
 				}
 			  },
@@ -582,22 +582,22 @@ guiFireworks.add(fworks, '冲击波');
 guiFireworks.add(fworks, '目标圆环');
 
 var guiParticles = gui.addFolder('尺寸');
-guiParticles.add(fworks, 'partCount').min(0).max(500).step(1);	
-guiParticles.add(fworks, 'partSpeed').min(1).max(100).step(1);
-guiParticles.add(fworks, 'partSpeedVariance').min(0).max(50).step(1);
-guiParticles.add(fworks, 'partWind').min(0).max(100).step(1);
-guiParticles.add(fworks, 'partFriction').min(0).max(50).step(1);
-guiParticles.add(fworks, 'partGravity').min(-20).max(20).step(1);
-guiParticles.add(fworks, 'flickerDensity').min(0).max(50).step(1);
+guiParticles.add(fworks, '烟花散开数量').min(0).max(500).step(1);	
+guiParticles.add(fworks, '速度').min(1).max(100).step(1);
+guiParticles.add(fworks, '烟花散开速度差').min(0).max(50).step(1);
+guiParticles.add(fworks, '部件风速').min(0).max(100).step(1);
+guiParticles.add(fworks, '烟花散开阻力').min(0).max(50).step(1);
+guiParticles.add(fworks, '重力方向').min(-20).max(20).step(1);
+guiParticles.add(fworks, '烟花密度').min(0).max(50).step(1);
 
 var guiColor = gui.addFolder('颜色');
-guiColor.add(fworks, 'hueMin').min(0).max(360).step(1);
-guiColor.add(fworks, 'hueMax').min(0).max(360).step(1);
-guiColor.add(fworks, 'hueVariance').min(0).max(180).step(1);
+guiColor.add(fworks, '色调最小值').min(0).max(360).step(1);
+guiColor.add(fworks, '色调最大值').min(0).max(360).step(1);
+guiColor.add(fworks, '色调差值').min(0).max(180).step(1);
 
 var guiOther = gui.addFolder('更多设置');
-guiOther.add(fworks, 'lineWidth').min(1).max(20).step(1);
-guiOther.add(fworks, 'clearAlpha').min(0).max(100).step(1);
-guiOther.add(fworks, 'clear').name('Clear');
+guiOther.add(fworks, '烟花粗细').min(1).max(20).step(1);
+guiOther.add(fworks, '消失时长').min(0).max(100).step(1);
+guiOther.add(fworks, 'clear').name('清空天空');
 
 gui.remember(fworks);
